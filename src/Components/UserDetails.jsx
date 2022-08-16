7f5a546268d4fb5ee2ecdcdef2c5e3df624665c6
J Khalaf <jwan.khalaf@outlook.com>
2022-08-16 10:55:31 +0100

    post user details

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,63 +1,89 @@
 import { useAuth } from "../security/authContext";
 import { useState } from "react";
 import { postUser } from "../Utility/api";
+import { Link, useNavigate } from "react-router-dom";
 
 const UserDetails = () => {
   const { currentUser } = useAuth();
   const [name, setName] = useState();
   const [username, setUsername] = useState();
-  const [gender, setGender] = useState();
+  const [gender, setGender] = useState("male");
   const [age, setAge] = useState();
   const [profileIcon, setProfileIcon] = useState();
-  const [skillsLevel, setSkillsLevel] = useState();
+  const [skillsLevel, setSkillsLevel] = useState("1");
   const [rating, setRating] = useState(0);
-  console.log(gender, name);
-
-  function handleSubmit() {}
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    const postBody = {
+      firebase_id: currentUser.uid,
+      name: name,
+      username: username,
+      age: Number(age),
+      gender: gender,
+      profile_icon: profileIcon,
+      skills_level: skillsLevel,
+      rating: 3,
+      event_id: 90,
+    };
+    postUser(postBody)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }
 
   return (
     <div className="userdetails">
-      <form onSubmit={handleSubmit}>
-        <h2>Create User</h2>
-        <label htmlFor="name">Name:</label>
-        <input
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          id="name"
-          type="text"
-          value={name}
-          required
-        />
+      {error ? (
+        <h3>could not post user details</h3>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h2>Create User</h2>
+          <label htmlFor="name">Name:</label>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            id="name"
+            type="text"
+            value={name}
+            required
+          />
 
-        <label htmlFor="username">Username:</label>
-        <input
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-          id="username"
-          type="text"
-          value={username}
-          required
-        />
-        <label for="age"> Please input your age:</label>
-        <input
-          onChange={(e) => {
-            setAge(e.target.value);
-          }}
-          type="number"
-          id="age"
-          name="age"
-          value={age}
-        />
-        <br></br>
-        <label>Gender: </label>
-        <select value={gender} onChange={(e) => setGender(e.target.value)}>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-        {/* <label for="gender1">Male</label>
+          <label htmlFor="username">Username:</label>
+          <input
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            id="username"
+            type="text"
+            value={username}
+            required
+          />
+          <label htmlFor="age"> Please input your age:</label>
+          <input
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+            type="number"
+            id="age"
+            name="age"
+            value={age}
+            max={100}
+            min={12}
+          />
+          <br></br>
+          <label>Gender: </label>
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          {/* <label for="gender1">Male</label>
         <input
           onChange={(e) => {
             setGender(e.target.value);
@@ -87,35 +113,35 @@ const UserDetails = () => {
           name="gender"
           value="mixed"
         /> */}
-        <br></br>
-        <label>
-          {" "}
-          Please select your skills level, 1 Star being Fun/Beginner, 5 Star
-          being Serious/Ex Pro:{" "}
-        </label>
+          <br></br>
+          <label>
+            {" "}
+            Please select your skills level, 1 Star being Fun/Beginner, 5 Star
+            being Serious/Ex Pro:{" "}
+          </label>
 
-        <select
-          value={skillsLevel}
-          onChange={(e) => setSkillsLevel(e.target.value)}
-        >
-          <option value="1">1 Star</option>
-          <option value="2">2 Star</option>
-          <option value="3">3 Star</option>
-          <option value="4">4 Star</option>
-          <option value="5">5 Star</option>
-        </select>
-        <label htmlFor="profileIcon">Profile Icon Image URL:</label>
-        <input
-          onChange={(e) => {
-            setProfileIcon(e.target.value);
-          }}
-          id="profileIcon"
-          type="text"
-          value={profileIcon}
-          required
-        />
+          <select
+            value={skillsLevel}
+            onChange={(e) => setSkillsLevel(e.target.value)}
+          >
+            <option value="1">1 Star</option>
+            <option value="2">2 Star</option>
+            <option value="3">3 Star</option>
+            <option value="4">4 Star</option>
+            <option value="5">5 Star</option>
+          </select>
+          <label htmlFor="profileIcon">Profile Icon Image URL:</label>
+          <input
+            onChange={(e) => {
+              setProfileIcon(e.target.value);
+            }}
+            id="profileIcon"
+            type="text"
+            value={profileIcon}
+            required
+          />
 
-        {/* <label for="skill1">1</label>
+          {/* <label for="skill1">1</label>
 
         <input
           onChange={(e) => {
@@ -166,8 +192,9 @@ const UserDetails = () => {
           name="skill"
           value="5"
         /> */}
-        <button>Add User/Submit</button>
-      </form>
+          <button>Add User/Submit</button>
+        </form>
+      )}
     </div>
   );
 };
