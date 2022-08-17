c6428dfe1caa305b6538f2d0149542f925780f53
beyardev <beyardev@gmail.com>
2022-08-17 11:55:36 +0100

    add loading

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,19 @@ import Map from "./Map";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getEvents } from "../Utility/api";
+import Loading from "./Loading";
 const Home = () => {
   const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
-    getEvents().then((events) => setEvents(events));
+    setIsLoading(true)
+    getEvents().then((events) => setEvents(events)).finally(()=>{
+      setIsLoading(false)
+    })
   }, []);
+  if(isLoading){
+    return <Loading />
+  }
   return (
     <div className="home">
       <div className="map-wrapper"><Map events={events}/></div>
@@ -25,10 +33,3 @@ const Home = () => {
 
 export default Home;
 
-// import { createTheme } from "@mui/material/styles";
-
-// const theme = createTheme({
-//   status: {
-//     primary: "#0a2145",
-//   },
-// });
